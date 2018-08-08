ab859fbbb478ee92e8490bc5f76fb5db355b7fd4
Ivan Trajkovic <32568927+Ivantrajko46@users.noreply.github.com>
2018-08-08 11:58:15 +0200

    keyboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ const printChar = {
 // printChar.equal
 
 // let = key = 'equal';
-// printChar[key]
+// printChar[key];
 
 $(function() {
 
@@ -24,6 +24,81 @@ $(function() {
     ekran2.html("");
     let lastOperationClick = false;
     let allInputs = [];
+
+    let shift = false;
+
+    // TASTATURA
+
+    window.addEventListener('keydown', function(event) {
+        console.log(event);
+        console.log(event.key);
+        console.log(event.keyCode);
+
+        if (event.keyCode === 16) {
+            shift = true;
+        }
+
+    });
+
+    window.addEventListener('keyup', function(event) {
+
+
+        if (event.keyCode === 16) {
+            shift = false;
+        }
+        if (event.keyCode === 187) {
+            if (shift) {
+                $('button[data-operation = "addition"]').trigger('click');
+            } else {
+                $('button[data-operation = "equal"]').trigger('click');
+            }
+        }
+
+        if (event.keyCode >= 96 && event.keyCode <= 105 || event.keyCode >= 48 && event.keyCode <= 57) {
+            let selector = `button[data-number="${event.key}"]`;
+            $(selector).trigger('click');
+        }
+
+        if (event.keyCode === 107) {
+            $('button[data-operation = "addition"]').trigger('click');
+        }
+
+        if (event.keyCode === 109 || event.keyCode === 189) {
+            $('button[data-operation = "subtraction"]').trigger('click');
+        }
+
+        if (event.keyCode === 106) {
+            $('button[data-operation = "multiplication"]').trigger('click');
+        }
+
+        if (event.keyCode === 111) {
+            $('button[data-operation = "division"]').trigger('click');
+        }
+
+        if (event.keyCode === 46) {
+            clear('entry');
+        }
+
+        if (event.keyCode === 27) {
+            clear('all');
+        }
+
+        if (event.keyCode === 13) {
+            defaultOperation("equal");
+            reset();
+        }
+
+        if (event.keyCode === 8) {
+            backspace();
+        }
+
+        if (event.keyCode === 190 || event.keyCode === 110) {
+            addPoint();
+        }
+
+    });
+
+    // KRAJ TASTATURE
 
     $('button[data-number]').click(function() {
         let number = $(this).data('number');
@@ -44,7 +119,6 @@ $(function() {
 
             case "clear-all":
                 clear('all');
-                reset();
                 break;
             case "clear-entry":
                 clear('entry');
@@ -81,6 +155,7 @@ $(function() {
         if (options === 'all') {
             ekran1.html('');
             ekran2.html('');
+            reset();
 
         } else if (options === 'entry') {
             ekran2.html('');
@@ -174,4 +249,16 @@ $(function() {
             ekran2.html(`${ekran2.html()}.`)
         }
     }
+
+    function getKey() {
+        for (let i = 0; i < Object.keys(printChar).length; i++) {
+            console.log(Object.keys(printChar)[i]);
+            if (Object.keys(printChar)[i] === 'equal') {
+			console.log(printChar(Object.keys(printChar)[i]));
+            		                                                              //JSON
+        }
+
+        
+    }
+}
 });
